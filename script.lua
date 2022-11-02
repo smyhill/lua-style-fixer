99c67c28d4de1be5139c0f2aa3215270b195a872
Simon Myhill <myhillsimon96@gmail.com>
2022-11-02 17:15:30 -0400

    Print Checked File List to Console

--- script.lua
+++ script.lua
@@ -1,12 +1,17 @@
-local GIT_DIFF_FILENAMES <const> = 'git diff --name-only'
-local PHP_FIXER <const> = 'php ./bin/php-cs-fixer fix --allow-risky=yes'    
+local GIT_DIFF_FILENAMES <const> = "git diff --name-only"
+local PHP_FIXER <const> = "php ./bin/php-cs-fixer fix --allow-risky=yes"    
 
 function fix_changed_files(files, command)
     local output = io.popen(files):read("*a")
+    local counter = 1
+    local files = ""
 
     for line in output:gmatch"[^\n]+" do
         os.execute(command .. ' ' .. line)
+        files = files .. counter .. "." .. " " .. line .. "\n"
+        counter = counter + 1
     end
+    io.write("Files Checked:" .. "\n" .. files)
 end
 
 -- get changed files in dir this command is run and then run php-cs-fixer on them
